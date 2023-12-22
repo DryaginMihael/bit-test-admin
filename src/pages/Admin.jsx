a658061cc35987563f64b29a4b131b060d68d156
DryaginMihael <dryaginmihael@yandex.ru>
2023-12-22 11:39:49 +0400

    refactor userList

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -3,6 +3,8 @@ import Pagination from "../components/Pagination";
 import UserList from "../components/UserList";
 import { USERS, PAGE_SIZE } from "../consts";
 import Drawer from "../components/Drawer";
+import SearchBar from "../UI/SearchBar";
+import useDebounce from "../hooks/useDebounce";
 
 function Admin() {
     const [currentPage, setCurrentPage] = useState(1);
@@ -10,6 +12,7 @@ function Admin() {
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
     const [currentUser, setCurrentUser] = useState(null);
     const [searchStr, setSearchStr] = useState("");
+    const handleSearch = useDebounce(setSearchStr, 200);
     const [sortType, setSortType] = useState("desc");
     const users = useMemo(() => {
         const offset = (currentPage - 1) * PAGE_SIZE;
@@ -32,10 +35,17 @@ function Admin() {
     };
 
     return (
-        <div className="Admin min-h-full bg-blue-gray sm:rounded-lg sm:m-6 py-6">
+        <div className="Admin min-h-full bg-blue-gray sm:rounded-lg sm:m-6 py-6 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold text-left mb-6">
+                Моя организация
+            </h1>
+            <hr className="-mx-4 sm:-mx-6 lg:-mx-8 border-slate-800" />
+            <h1 className="text-3xl font-bold text-left mt-4">
+                Пользователи
+            </h1>
+            <SearchBar onSearch={handleSearch} />
             <UserList
                 users={users}
-                onSearch={setSearchStr}
                 sortType={sortType}
                 onSort={setSortType}
                 openDrawer={openDrawer}
