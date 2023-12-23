01746327d16fcc49d56d0e1c9f2a4f6aa116875b
DryaginMihael <dryaginmihael@yandex.ru>
2023-12-23 12:54:09 +0400

    fix test with empty search

--- src/pages/Admin.test.js
+++ src/pages/Admin.test.js
@@ -11,10 +11,15 @@ describe("ADMIN TESTS", () => {
 
     test("EMPTY SEARCH", async () => {
         render(<Admin />);
+
         const searchBar = screen.getByPlaceholderText(/поиск/i);
-        userEvent.type(searchBar, '/**80727390)(()~');
-        const usersTable = await screen.findByRole('table');
-        expect(usersTable).toBeNull();
+        await userEvent.type(searchBar, '/**80727390)(()~');
+        await screen.findByRole('table');
+
+        await new Promise((resolve) => setTimeout(resolve, 200)); // wait input delay
+
+        expect(screen.queryByRole('table')).toBeNull(); // table doesn't exist
+        expect(screen.getByText('Ничего не найдено')).toBeInTheDocument(); // Not found exists
         
     })
 })
